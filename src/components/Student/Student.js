fdad6f97661f27c5b5b7c936a5d0a2350a988bea
Vladislav Kozhevatov <envydef@gmail.com>
2021-05-18 12:59:08 +0300

    fix: links

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -12,8 +12,8 @@ function Student(props) {
                 <p className="student__paragraph">Я живу в Москве, закончил факультет менеджмента MIRBIS. У меня есть жена
 и два сына. Я люблю фэнтези и увлекаюсь кулинарией.С 2016 года развиваю собственный бизнес. Полученые знания хочу направить для развития нового направления в компании. </p>
             <ul className="student__social">
-                <a className="student__social-item" href="#facebook">Facebook</a>
-                <a className="student__social-item" href="#github">Github</a>
+                <a className="student__social-item" target='_blank' rel='noreferrer' href='https://www.facebook.com/vlad.kozhevatov'>Facebook</a>
+                <a className="student__social-item" target='_blank' rel='noreferrer' href='https://github.com/EnvyvnE'>Github</a>
             </ul>
             </div>
             <img src={avatar} alt="avatar" className="student__image" />
